f0b14ed1a6e6b17ec3c2535ce8649792069c41f4
EduMFreire <eduardodemendoncafreire@gmail.com>
2025-10-03 09:57:02 -0300

    Voltando do começo: troca tudo por um Hello, World com TypeScript.

---
+++ Tentativa10_02/src/main.ts
@@ -0,0 +1,8 @@
+console.log("Hello, world from TypeScript!");
+/*
+const app = document.querySelector<HTMLElement>("#app");
+
+if (app) {
+  app.textContent = "Hello, world from TypeScript!";
+}
+*/
