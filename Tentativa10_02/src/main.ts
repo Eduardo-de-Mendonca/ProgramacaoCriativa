84adc06a65f63fd956cf05b267a28e1e023c06b5
EduMFreire <eduardodemendoncafreire@gmail.com>
2025-10-03 11:43:17 -0300

    Add um dos meus sketches.

--- Tentativa10_02/src/main.ts
+++ Tentativa10_02/src/main.ts
@@ -1,5 +1,4 @@
-//console.log("Hello, world from TypeScript!");
-
+/*
 import p5 from "p5";
 
 const sketch = (p: p5) => {
@@ -9,6 +8,41 @@ const sketch = (p: p5) => {
 
   p.draw = () => {
     // nothing for now
+  };
+};
+
+new p5(sketch);
+*/
+
+import p5 from "p5";
+
+let w: number;
+let h: number;
+let line_proportion = 0.1;
+let iterations = 32;
+
+const sketch = (p: p5) => {
+  p.setup = () => {
+    p.createCanvas(p.windowWidth, p.windowHeight);
+    w = p.width;
+    h = p.height;
+
+    p.background("white");
+
+    let x = line_proportion * w;
+    for (let i = 1; i < iterations; i++) {
+      vertical_infinite_line(p, x);
+      x = p.map(line_proportion, 0, 1, x, w);
+    }
+  };
+
+  const vertical_infinite_line = (p: p5, x: number) => {
+    let x1 = x;
+    let x2 = x;
+    let y1 = 0;
+    let y2 = h;
+
+    p.line(x1, y1, x2, y2);
   };
 };
 
