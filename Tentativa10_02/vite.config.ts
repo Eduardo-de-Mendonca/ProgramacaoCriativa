96770fa2eec50751ea52b44ca4c4a6c9a85532df
EduMFreire <eduardodemendoncafreire@gmail.com>
2025-10-03 10:57:13 -0300

    Muda o base, para ver se funciona no GitHub.

---
+++ Tentativa10_02/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+
+/*
+export default defineConfig({
+  base: "/ProgramacaoCriativa/Tentativa10_02/dist/",
+});
+*/
+
+export default defineConfig({
+  base: "/Tentativa10_02/dist/",
+});
