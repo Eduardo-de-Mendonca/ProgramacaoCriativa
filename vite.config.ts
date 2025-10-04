71459b1b7fd9f4052a1399573dcb165eb9fd37e8
EduMFreire <eduardodemendoncafreire@gmail.com>
2025-10-04 15:18:49 -0300

    Renomeia a pasta e move tsconfig.json para ProgramacaoCriativa.

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 // Detect which folder we're building
-// e.g. "npm run build Tentativa10_02"
-const project = process.env.TENTATIVA || "Tentativa10_02"; // default
+const project = process.env.ARTEFATO || "Artefato10_02"; // default
 
 export default defineConfig({
   root: resolve(__dirname, project),
